8154a4256d3c37a129ec4043ae689d74a3c5f132
iCrawl <buechler.noel@outlook.com>
2022-09-01 18:28:16 +0200

    feat: add edge rules

---
+++ src/edge.js
@@ -0,0 +1,13 @@
+module.exports = {
+	rules: {
+		"import/extensions": 0,
+		"no-restricted-globals": 0,
+		"n/prefer-global/buffer": [2, "always"],
+		"n/prefer-global/console": [2, "always"],
+		"n/prefer-global/process": [2, "always"],
+		"n/prefer-global/text-decoder": [2, "always"],
+		"n/prefer-global/text-encoder": [2, "always"],
+		"n/prefer-global/url": [2, "always"],
+		"n/prefer-global/url-search-params": [2, "always"],
+	},
+};
