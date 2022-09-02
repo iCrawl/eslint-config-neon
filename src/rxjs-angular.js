2a145b7ee49f12fa2b3e969ef3d6e308d70db91e
iCrawl <buechler.noel@outlook.com>
2022-09-02 18:34:25 +0200

    feat: rxjs rules

---
+++ src/rxjs-angular.js
@@ -0,0 +1,9 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	plugins: ["rxjs-angular"],
+	rules: {
+		"rxjs-angular/prefer-async-pipe": 0,
+		"rxjs-angular/prefer-composition": 0,
+		"rxjs-angular/prefer-takeuntil": 0,
+	},
+};
