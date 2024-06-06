93cd5804dfcff87b4255c5cb125905fe2155ebaa
Jeroen Claassens <support@favware.tech>
2024-06-06 22:23:59 +0200

    style: formatting

--- scripts/fix-tsup-type-files.ts
+++ scripts/fix-tsup-type-files.ts
@@ -1,7 +1,7 @@
 import { findFilesRecursivelyStringEndsWith } from "@sapphire/node-utilities";
 import { rename } from "node:fs/promises";
 
-const distUrl = new URL('../dist/esm/', import.meta.url);
+const distUrl = new URL("../dist/esm/", import.meta.url);
 
 for await (const file of findFilesRecursivelyStringEndsWith(distUrl, ".d.ts")) {
 	await rename(file, file.replace(/\.d\.ts$/, ".d.mts"));
