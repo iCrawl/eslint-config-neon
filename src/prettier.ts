c2bb584f0e802d28843d92d39a799f6517dbad01
Jeroen Claassens <jeroen.claassens@rws.nl>
2025-03-17 17:03:36 +0100

    fix: re-add stylistic and prettier compatibility (#16)
    Apparently the lists needed an exhaustive diff as my assumption that I could just remove the whole list was wrong.
    
    Diff of the 2 lists: https://www.diffchecker.com/ltmHbE2r/

--- src/prettier.ts
+++ src/prettier.ts
@@ -6,14 +6,27 @@ const rules: TSESLint.FlatConfig.Rules = {
 
 	"no-inline-comments": 0,
 
-	// Stylistic rules enabled by /jsx or /react that should be disabled when using Prettier
-	"@stylistic/jsx/jsx-one-expression-per-line": 0,
+	// Stylistic rules enabled other configs that should be disabled when using Prettier
+	"@stylistic/js/line-comment-position": 0,
+	"@stylistic/js/lines-around-comment": 0,
+	"@stylistic/js/max-len": 0,
+	"@stylistic/js/no-confusing-arrow": 0,
+	"@stylistic/js/no-mixed-operators": 0,
+	"@stylistic/js/no-tabs": 0,
+	"@stylistic/js/quotes": 0,
 	"@stylistic/jsx/jsx-newline": 0,
+	"@stylistic/jsx/jsx-one-expression-per-line": 0,
 	"@stylistic/jsx/jsx-props-no-multi-spaces": 0,
 	"@stylistic/jsx/jsx-tag-spacing": 0,
 	"@stylistic/jsx/jsx-wrap-multilines": 0,
-	
-	"@stylistic/js/line-comment-position": 0,
+	"@stylistic/lines-around-comment": 0,
+	"@stylistic/max-len": 0,
+	"@stylistic/no-confusing-arrow": 0,
+	"@stylistic/no-mixed-operators": 0,
+	"@stylistic/no-tabs": 0,
+	"@stylistic/quotes": 0,
+	"@stylistic/ts/lines-around-comment": 0,
+	"@stylistic/ts/quotes": 0,
 };
 
 const config: TSESLint.FlatConfig.ConfigArray = [
